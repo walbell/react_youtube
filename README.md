d190dde83e408e9be0f5fbf392ff445666d21e7d
Walter Bellante <walbell88@hotmail.it>
2016-02-11 14:28:40 +0000

    First commit

--- README.md
+++ README.md
@@ -1,21 +1,8 @@
-# ReduxSimpleStarter
+# React Youtube Client
 
-###Getting Started###
+This is a simple Youtube client built with React
 
-There are two methods for getting started with this repo.
-
-####Familiar with Git?#####
-Checkout this repo, install depdencies, then start the gulp process with the following:
-
-```
-	> git clone git@github.com:StephenGrider/ReduxSimpleStarter.git
-	> cd ReduxSimpleStarter
-	> npm install
-	> npm start
-```
-
-####Not Familiar with Git?#####
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+###Install and run project###
 
 ```
 	> npm install
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+//import components
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyC_Qgnfh_ZlpHJQhyNk9a1B05YayU02U-M';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surf');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key:API_KEY, term: term}, videos => {
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+
+		const VideoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
+		return (<div>
+			<SearchBar 
+				onTextChange = { VideoSearch } />
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList
+				onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+				videos={this.state.videos} />
+		</div>);
+	}
+}
+
+//it is like
+/**
+ * const App = function () {
+ *       return <div> Hi! </div>;
+ * }
+ */
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.getElementsByClassName('container')[0]);
